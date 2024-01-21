516ebfe688e7f015b2f5d55044b384da7ac222d3
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2024-01-21 08:30:00 +0000

    Made minor changes.
    • Rewrite init.recovery.qcom.rc
    • Removed old kludge and add new kludge about preventing the stock ROM from replacing TWRP.

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -1,20 +1,29 @@
 #!/sbin/sh
 #
-# TODO: this kludge is needed to prevent issues with mounting
-# system and vendor in some zip installers and in the gui
+# TODO: this kludge is needed to prevent removing stock recovery file in /vendor
+# to prevent the stock ROM from replacing TWRP.
 #
-sleep 1
-mount -w /vendor > /dev/null 2>&1
-mount -w /odm > /dev/null 2>&1
-mount -w /product > /dev/null 2>&1
-mount -w /system_ext > /dev/null 2>&1
-mount -w /system_root > /dev/null 2>&1
 
-sleep 1
-umount /vendor > /dev/null 2>&1
-umount /odm > /dev/null 2>&1
-umount /product > /dev/null 2>&1
-umount /system_ext > /dev/null 2>&1
-umount /system_root > /dev/null 2>&1
+mkdir -p "/tmp/vendor";
+mkdir -p "/tmp/system";
+mount -w "/dev/block/mapper/vendor" "/tmp/vendor";
+mount -w "/dev/block/mapper/system" "/tmp/system";
 
-exit 0
+# Android 12+
+if [ -f "/tmp/vendor/recovery-from-boot.p" ]; then
+  echo "I:postrecoveryboot: Removing stock recovery file in /vendor to prevent the stock ROM from replacing TWRP." >> /tmp/recovery.log;
+  rm "/tmp/vendor/bin/install-recovery.sh";
+  rm "/tmp/vendor/etc/init/vendor_flash_recovery.rc";
+  rm "/tmp/vendor/recovery-from-boot.p";
+elif [ -f "/tmp/system/system/recovery-from-boot.p" ]; then
+  # Android 11
+  rm "/tmp/system/system/recovery-from-boot.p" ];
+  rm "/tmp/system/system/bin/install-recovery.sh" ];
+fi;
+
+umount "/tmp/vendor";
+umount "/tmp/system";
+rm -r "/tmp/vendor";
+rm -r "/tmp/system";
+
+exit 0;
